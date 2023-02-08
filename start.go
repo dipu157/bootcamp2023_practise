beae8cc9c58e56f7d7943743e9d7bf2a3a5d6e3c
dipu157 <dipu157@gmail.com>
2023-02-08 18:48:12 +0600

    rune data type check

--- start.go
+++ start.go
@@ -1,17 +1,23 @@
 package main
+
 import (
-"fmt" 
-"reflect"
+	"fmt"
+	// "reflect"
 )
 
 func main() {
-  var student1 string = "John" //type is string
-  var student2 = "Jane" //type is inferred
-  x := 2 //type is inferred
+	// var student1 string = "John" //type is string
+	// var student2 = "Jane" //type is inferred
+	// x := 2 //type is inferred
 
-  fmt.Println(student1)
-  fmt.Println(student2)
-  fmt.Println(x)
-  fmt.Println("Type of student2:",reflect.TypeOf(student2))
-  fmt.Println("Type of x:",reflect.TypeOf(x))
+	// fmt.Println(student1)
+	// fmt.Println(student2)
+	// fmt.Println(x)
+	// fmt.Println("Type of student2:",reflect.TypeOf(student2))
+	// fmt.Println("Type of x:",reflect.TypeOf(x))
+
+	var chr rune
+	chr = '$'
+
+	fmt.Println(chr)
 }
