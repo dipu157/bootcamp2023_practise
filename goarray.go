1bad757366af96504369020b26c95b78f54c3fb2
dipu157 <dipu157@gmail.com>
2023-02-09 12:25:09 +0600

    slice and map practise

--- goarray.go
+++ goarray.go
@@ -7,16 +7,41 @@ import (
 
 func main() {
 
-	var student [3] string
+	// var student [3]string
 
-	student[0] = "Amin"
-	student[1] = "Babul"
-	student[2] = "Sifat "
+	// student[0] = "Amin"
+	// student[1] = "Babul"
+	// student[2] = "Sifat "
 
-	employees := [3]string{"arwa","Bokul","rahi"}
+	// employees := [3]string{"arwa", "Bokul", "rahi"}
 
-	fmt.Println(student)
-	fmt.Println(len(student))
-	fmt.Println(student[2])
-	fmt.Println(employees)
+	// fmt.Println(student)
+	// fmt.Println(len(student))
+	// fmt.Println(student[2])
+	// fmt.Println(employees)
+
+	// Slice Example
+
+	// var name []string
+	// name = append(name, "abul", "kobul")
+
+	// fmt.Println(name)
+
+	// name = append(name, "Samsu", "khan")
+
+	// fmt.Println(name)
+
+	//x := make([]float64, 5, 10)
+
+	// arr := [5]float64{1, 2, 3, 4, 5}
+	// x := arr[0:5]
+
+	// fmt.Println(x)
+
+	// Map Example
+
+	x := make(map[string]int)
+	x["key"] = 10
+	fmt.Println(x["key"])
+
 }
