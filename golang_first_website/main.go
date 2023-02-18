d7921efb5475b7ba9859dbc0d6c28893b20eb611
dipu157 <dipu157@gmail.com>
2023-02-18 10:41:28 +0600

    insert data to mysql database with simple method

--- golang_first_website/main.go
+++ golang_first_website/main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+var db *sql.DB
+var err error
+
+func init() {
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	db, err = sql.Open("mysql", "root:33133@tcp(127.0.0.1:3308)/hosting_db")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+	// defer db.Close()
+
+	fmt.Println("DB Connection Successfully")
+}
 
 func main() {
 
@@ -63,7 +87,28 @@ func request(w http.ResponseWriter, r *http.Request) {
 	company := r.FormValue("company")
 	email := r.FormValue("email")
 
-	fmt.Println(name, company, email)
+	// fmt.Println(name, company, email)
 
-	fmt.Fprintf(w, `received`)
+	// fmt.Fprintf(w, `received`)
+
+	// r.ParseForm()
+
+	// for key, val := range r.Form {
+	// 	fmt.Println(key, val)
+	// }
+
+	// fmt.Fprintf(w, `OK`)
+
+	qs := "INSERT INTO `request` (`name`,`company`,`email`,`status`)  VALUES ( '%s', '%s', '%s', 1 )"
+	sql := fmt.Sprintf(qs, name, company, email)
+
+	// perform a db.Query insert
+	insert, err := db.Query(sql)
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// be careful deferring Queries if you are using transactions
+	defer insert.Close()
 }
