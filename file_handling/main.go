bd15464702b0299a8275e25e12751b717dd19d6c
dipu157 <dipu157@gmail.com>
2023-02-20 13:25:22 +0600

    caleb doxbey book practise____for loop

--- file_handling/main.go
+++ file_handling/main.go
@@ -3,25 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 
-	// dir, err := os.Getwd()
+	dir, err := os.Getwd()
 
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
 	// fmt.Println(dir)
 
 	// isErr := createFile("second_file.txt", "Yap, Golang is awesome")
 	// fmt.Println(isErr)
 
-	err := os.Mkdir("test_folder", 0777)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	// err := os.Mkdir("test_folder", 0777)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	base := filepath.Base(dir)
+
+	absolutePath, _ := filepath.Abs("test_folder")
+
+	newPath := filepath.Join(absolutePath, "..", "..", "lecture_25")
+	fmt.Println(base)
+	fmt.Println(absolutePath)
+	fmt.Println(newPath)
+	os.Mkdir(newPath, 777)
 
 }
 
