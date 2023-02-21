e6151b5f5ad6d340ef390a214d04f310b7ec93f0
dipu157 <dipu157@gmail.com>
2023-02-21 11:21:01 +0600

    understanding defer

--- book practice/functions/main.go
+++ book practice/functions/main.go
@@ -2,33 +2,28 @@ package main
 
 import "fmt"
 
-func add(args ...int) int {
-	total := 0
-	for _, v := range args {
-		total += v
-	}
-	return total
-}
+// func add(args ...int) int {
+// 	total := 0
+// 	for _, v := range args {
+// 		total += v
+// 	}
+// 	return total
+// }
 
 func main() {
-	//fmt.Println(add(1, 2, 3, 10, 20))
-	//fmt.Println(f1())
 
-	x := 0
-	increment := func() int {
-		x++
-		return x
-	}
-	fmt.Println(increment())
-	fmt.Println(increment())
-	fmt.Println(increment())
-	fmt.Println(increment())
+	second()
+	defer first()
+	third()
+
 }
 
-// func f1() int {
-// 	return f2()
-// }
-
-// func f2() int {
-// 	return 10
-// }
+func first() {
+	fmt.Println("1st")
+}
+func second() {
+	fmt.Println("2nd")
+}
+func third() {
+	fmt.Println("Third")
+}
