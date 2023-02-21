3ae246d517345720219300298c95db158fd04f16
dipu157 <dipu157@gmail.com>
2023-02-21 10:41:16 +0600

    closure practice

--- book practice/functions/main.go
+++ book practice/functions/main.go
@@ -11,8 +11,18 @@ func add(args ...int) int {
 }
 
 func main() {
-	fmt.Println(add(1, 2, 3, 10, 20))
+	//fmt.Println(add(1, 2, 3, 10, 20))
 	//fmt.Println(f1())
+
+	x := 0
+	increment := func() int {
+		x++
+		return x
+	}
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(increment())
 }
 
 // func f1() int {
