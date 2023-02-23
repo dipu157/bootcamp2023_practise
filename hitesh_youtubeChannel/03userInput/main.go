bebe29558787ef988ef3f93278c70466ecaa9950
dipu157 <dipu157@gmail.com>
2023-02-23 16:22:48 +0600

    convert string to int

--- hitesh_youtubeChannel/03userInput/main.go
+++ hitesh_youtubeChannel/03userInput/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,4 +18,12 @@ func main() {
 	input, _ := reader.ReadString('\n')
 	fmt.Println("Thanks for rating", input)
 	fmt.Printf("Type of the rating %T", input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added 1 to your rating: ", numRating+1)
+	}
 }
