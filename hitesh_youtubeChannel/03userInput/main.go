fa5fdc18814899f544bace3d25706bf1858a150c
dipu157 <dipu157@gmail.com>
2023-02-23 15:39:23 +0600

    user input by bufio package

---
+++ hitesh_youtubeChannel/03userInput/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating of our pizza: ")
+
+	//comma ok
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating", input)
+	fmt.Printf("Type of the rating %T", input)
+}
