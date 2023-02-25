53008572bca600adc2e98842a3d3afce3585c943
dipu157 <dipu157@gmail.com>
2023-02-25 18:06:16 +0600

    struct practice

---
+++ hitesh_youtubeChannel/11myStruct/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Welcome to Struct Tutorial")
+
+	hitesh := User{"hitesh", "abc@mail.com", true, 10}
+	fmt.Println(hitesh)
+	fmt.Printf("My details are:%+v\n", hitesh)
+	fmt.Printf("Name is %v and Email is%v \n", hitesh.Name, hitesh.Email)
+
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
+}
