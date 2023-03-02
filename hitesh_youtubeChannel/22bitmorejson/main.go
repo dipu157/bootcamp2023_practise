eb51e102807f08b562043baf0e4af65cc4caea2c
dipu157 <dipu157@gmail.com>
2023-03-02 21:01:39 +0600

    consume json data

--- hitesh_youtubeChannel/22bitmorejson/main.go
+++ hitesh_youtubeChannel/22bitmorejson/main.go
@@ -16,7 +16,8 @@ type courses struct {
 func main() {
 	fmt.Println("Welcome To JSON")
 
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -33,3 +34,48 @@ func EncodeJson() {
 	}
 	fmt.Printf("%s\n", finalJson)
 }
+
+func DecodeJson() {
+	jsonDataFromWeb := []byte(`
+	{
+		"coursename": "VueJs",
+		"Price": 99,
+		"website": "abcOnline.com",
+		"tags": ["web-dev","js"]
+	}
+`)
+
+	var lcoCourse courses
+
+	checkValid := json.Valid(jsonDataFromWeb)
+
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		fmt.Printf("%#v", lcoCourse)
+	} else {
+		fmt.Println("JSON was not valid")
+	}
+
+	// some cases for add data as key value
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("key is %v and value is %v and type is %T\n", k, v, v)
+	}
+}
+
+/*
+You skipped a very important topic there. var lcoCourse course definition style is new to users and deserves further explanation.
+
+var lcoCourse course is same as var lcoCourse = course{}.
+From what I understand, we create here an instance of the course struct that was defined earlier. Yet, this instant is updated once the Unmarshal function is called and &lcoCourse is used. So, lcoCourse in this scenario is mutable (can be changed)
+
+Another very important note: var lcoCourse = course{} is completely different than var lcoCourse = []course{}. The second one is creating a new method from my research that is immutable (cannot be changed). Therefore we use course{} here.
+
+Feel free to try it yourself and also print the lcoCourse before and after Unmarshal is applied. You will see the difference. I hope Hitesh will explain this in details in upcoming classes.
+
+*/
