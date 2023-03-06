2fe9b2c5a98c1265ab343004d3b3fb6de2840891
dipu157 <dipu157@gmail.com>
2023-03-06 12:53:10 +0600

    building api

---
+++ hitesh_youtubeChannel/24buildapi/main.go
@@ -0,0 +1,25 @@
+package main
+
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	FullName string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// fake DB
+var courses []Course
+
+// middleware, helper - file
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
+func main() {
+
+}
